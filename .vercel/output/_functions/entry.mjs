f8f3f04f4aaa9ecb8af4d8b8a9e2b421aeb59456
MorTruAdam <adam.r.merrill@gmail.com>
2025-11-21 16:15:06 -0600

    chore: update package dependencies

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { r as renderers } from './chunks/_@astro-renderers_DsNcxPmq.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_k4WljfAi.mjs';
-import { manifest } from './manifest_BLq5B9eb.mjs';
+import { manifest } from './manifest_0IBKdlTY.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -21,7 +21,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "4d31a286-cf5a-4442-a1e3-c9c8b57ef8cb",
+    "middlewareSecret": "4131c6f8-1c9d-4b79-a86b-bfaa4cb430d1",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
