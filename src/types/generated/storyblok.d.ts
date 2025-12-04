a5bced2b8b2ff8bb2550089236140c72ec74a239
MorTruAdam <adam.r.merrill@gmail.com>
2025-12-04 16:25:49 -0600

    fix: update generated types to match schema

--- src/types/generated/storyblok.d.ts
+++ src/types/generated/storyblok.d.ts
@@ -1,7 +1,7 @@
 /**
  * 🤖 AUTO-GENERATED FILE. DO NOT EDIT.
  * Source: Storyblok Management API
- * Generated: 2025-12-04T22:20:04.323Z
+ * Generated: 2025-12-04T22:25:44.083Z
  *
  * @description
  * This file contains TypeScript type definitions for all Storyblok components.
